fbc05fda273cb667ab3995ae75b611b51d85efc0
grittothomas <grittothomas@gmail.com>
2024-03-01 15:15:22 +0530

    old app

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello():
-    return render_template('webassets/index.html')
-
+    return 'Hello, World! By Flask'
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
